f8b91a8a9514894509604860ec2d16cb4e3a1da6
AbdelrahmanElzayat <bodyelmajec@gmail.com>
2024-04-30 16:01:21 +0300

    edit brand to swiper

--- src/components/HomeComponents/blogs/LastBlogs.js
+++ src/components/HomeComponents/blogs/LastBlogs.js
@@ -12,7 +12,7 @@ const LastBlogs = () => {
         </div>
         <Container>
             <div className='seeMore'>
-                <a href='##'>عرض الجميع
+                <a href='/blogs'>عرض الجميع
                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
                     <path opacity="0.6" d="M12.8333 6.41665L8.25 11L12.8333 15.5833" stroke="white" strokeWidth="1.875" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
--- src/components/HomeComponents/brandsSection/Brands.js
+++ src/components/HomeComponents/brandsSection/Brands.js
@@ -1,31 +1,81 @@
-import React from 'react'
-import './Brands.css'
-import { Container } from 'react-bootstrap'
-const Brands = () => {
-    const imgs = 
-    [
-        {className:'brand1'},
-        {className:'brand2'},
-        {className:'brand3'},
-        {className:'brand4'},
-        {className:'brand5'},
-        {className:'brand6'}
-    ]
-  return (
-    <div className='Brands'>
-        <Container>
-            <div className='BrandsContainer'>
-                {
-                    imgs.map((item , index)=>(
-                        <div className={`BrandImg_test ${item.className}`} key={index}>
-                            
-                        </div>          
-                    ))
-                }
-            </div>
-        </Container>
-    </div>
-  )
-}
+import React, { useEffect, useState } from "react";
+import "./Brands.css";
+import { Container } from "react-bootstrap";
 
-export default Brands
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+// import required modules
+import { Autoplay } from "swiper/modules";
+
+const Brands = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  const slidesPerView = isMobile ? 3 : 6;
+  const imgs = [
+    { className: "brand1" },
+    { className: "brand2" },
+    { className: "brand3" },
+    { className: "brand4" },
+    { className: "brand5" },
+    { className: "brand6" },
+  ];
+  return (
+    <div className="Brands">
+      <Container>
+        <div className="BrandsContainer">
+          {/* {imgs.map((item, index) => (
+            <div
+              className={`BrandImg_test ${item.className}`}
+              key={index}
+            ></div>
+          ))} */}
+
+          <Swiper
+            slidesPerView={slidesPerView}
+            spaceBetween={30}
+            // pagination={{
+            //   clickable: true,
+            // }}
+            // modules={[Pagination]}
+            className="mySwiper"
+
+
+            centeredSlides={isMobile && true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            modules={[Autoplay]}
+            loop={true}
+          >
+            {imgs.map((item, index) => (
+              <SwiperSlide>
+                <div
+                  className={`BrandImg_test ${item.className}`}
+                  key={index}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Brands;
